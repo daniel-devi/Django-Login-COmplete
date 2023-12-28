283cc894cf1a3b3bc692c17ce5af26fff200fcd7
Daniel Agufenwa <142681753+daniel-devi@users.noreply.github.com>
2023-12-28 12:40:16 +0000

    settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'agufenwaobinna@gmail.com'
-EMAIL_HOST_PASSWORD = 'inlx dfus chwr ubds'
+EMAIL_HOST_USER = '####################'
+EMAIL_HOST_PASSWORD = '###############'
